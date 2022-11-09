c40283b4a16e59bf97a48afca9142fd10c448f4d
sebastienmoulin <sebastien.m.web@gmail.com>
2022-11-09 09:44:01 +0100

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = process.env.APP_PORT ?? 5005;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+const users = require("./users");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/users", users.getUsers);
+app.get("/api/users/:id", users.getUsersId);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
